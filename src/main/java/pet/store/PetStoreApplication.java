90a577a34b565954f2628370d1d2f548dbdfcd54
ProjectGrantwood <justinphillipturner@gmail.com>
2023-05-26 14:51:54 -0400

    initial commit

---
+++ src/main/java/pet/store/PetStoreApplication.java
@@ -0,0 +1,15 @@
+package pet.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class PetStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetStoreApplication.class, args);
+	}
+
+}
